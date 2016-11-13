8910b0c7e7d4d2e4ae95bf19c807ce469dec01bf
erictoddperry@gmail.com <erictoddperry@gmail.com>
2016-11-12 21:30:52 -0500

    update

---
+++ Assets/UpdateCloseOnEscape.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateCloseOnEscape : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+         if (Application.platform == RuntimePlatform.Android)
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
